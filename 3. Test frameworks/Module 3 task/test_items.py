042751b4e2e7eba5c96325487ca90698cf3c9512
pankotek <ignat2404@gmail.com>
2020-05-27 17:06:59 +0300

    Updated last task

--- 3. Test frameworks/Module 3 task/test_items.py
+++ 3. Test frameworks/Module 3 task/test_items.py
@@ -1,4 +1,4 @@
-import time
+# import time
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -6,5 +6,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_cart_existence(browser):
     browser.get(link)
+#    time.sleep(5)
     assert browser.find_element_by_css_selector('.btn-add-to-basket')
-    time.sleep(5)
+
